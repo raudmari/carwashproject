6663fd6b03ba9da1413e228ed38dfb759611b747
Marion <marion@marion.ee>
2021-05-03 10:30:54 +0300

    Initial commit

---
+++ src/main/java/com/example/valiit/carwashproject/CarWashProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.valiit.carwashproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWashProjectApplication.class, args);
+	}
+
+}
