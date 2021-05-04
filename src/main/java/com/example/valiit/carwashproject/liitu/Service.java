0ee580f816455792cdfb274696cbd58632386f5f
Alari Kukk <alarikukk@hotmail.com>
2021-05-04 12:15:08 +0300

    3rd

--- src/main/java/com/example/valiit/carwashproject/liitu/Service.java
+++ src/main/java/com/example/valiit/carwashproject/liitu/Service.java
@@ -2,18 +2,23 @@ package com.example.valiit.carwashproject.liitu;
 
 import com.example.valiit.carwashproject.DTO.AccountJoin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @org.springframework.stereotype.Service
 public class Service {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private Repository repository;
 
     public void newAccountJoin(AccountJoin request) {
 
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        request.setPassword(encodedPassword);
+
         repository.newAccountJoin(request);
-
-
 
     }
 
