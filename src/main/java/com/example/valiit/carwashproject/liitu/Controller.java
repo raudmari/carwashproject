02dbc219f240aa2fc8182e4ea74ffe24baf6fd70
Alari Kukk <alarikukk@hotmail.com>
2021-05-10 16:08:32 +0300

    liitumine,

--- src/main/java/com/example/valiit/carwashproject/liitu/Controller.java
+++ src/main/java/com/example/valiit/carwashproject/liitu/Controller.java
@@ -4,6 +4,7 @@ package com.example.valiit.carwashproject.liitu;
 import com.example.valiit.carwashproject.DTO.AccountJoin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class Controller {
 
 
     //http://localhost:9090/carWash/newAccountJoin
+    @CrossOrigin
     @PostMapping("carWash/newAccountJoin")
     public void newAccountJoin(@RequestBody AccountJoin request) {
         service.newAccountJoin(request);
